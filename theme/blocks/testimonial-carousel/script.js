c23bca937cf8f0c78a40c500a0c4d66c64204f97
Barry Tickle <barry.tickle12@gmail.com>
2026-08-06 19:49:32 +0100

    Make testimonial carousel support single or multiple testimonials
    Replaces the fixed quote + decorative avatars with a repeater of full
    testimonials. One testimonial renders centred and static; two or more
    slide via Swiper, with pagination dots styled to match the theme.

---
+++ theme/blocks/testimonial-carousel/script.js
@@ -0,0 +1,15 @@
+import Swiper from "swiper";
+import { Pagination } from "swiper/modules";
+
+document.querySelectorAll(".testimonial-carousel-swiper").forEach((el) => {
+	new Swiper(el, {
+		modules: [Pagination],
+		loop: true,
+		slidesPerView: 1,
+		spaceBetween: 32,
+		pagination: {
+			el: el.querySelector(".swiper-pagination"),
+			clickable: true,
+		},
+	});
+});
