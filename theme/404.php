bcaa50d30c298c56197f5c5f8dd2eb8647804797
Barry Tickle <barry.tickle12@gmail.com>
2026-08-19 15:59:01 +0100

    Use not found component for 404 page

--- theme/404.php
+++ theme/404.php
@@ -5,5 +5,16 @@
  * @since Timberland 2.1.0
  */
 
-$context = Timber::context();
+$context                     = Timber::context();
+$context['not_found_fields'] = array(
+	'heading_highlight' => '404',
+	'heading'           => 'Not Found',
+	'body'              => 'It seems you got a little bit lost.',
+	'cta_link'          => array(
+		'url'    => home_url( '/' ),
+		'title'  => 'Back to homepage',
+		'target' => '',
+	),
+);
+
 Timber::render( '404.twig', $context );
